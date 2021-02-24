29bf382e9c6461775a56da228a94259a6c59d003
ö <daveyeb@gmail.com>
2021-02-24 18:12:13 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,9 +9,9 @@ int main()
 
 	cash = malloc(sizeof(wheels));
 	money = malloc(sizeof(plugboard));
-	int a = config_wheel_order(cash, "III II I C_THIN");
+	int a = config_wheel_order(cash, "C_THIN III II I");
 	a = config_start_pos_rotors(cash, "V E Q");
-	a = config_plug_connections(money, "");
+	a = config_plug_connections(money, "EJ OY IV AQ KW FX MT PS LU BD");
 	a = config_reflector_wiring(cash, "BETA", "IU AS DV GL FT OX EZ CH MR KN BQ PW");
 	a = config_ring_settings(cash, "1 1 1");
 	a = encrypt(cash, money, "FRUGAL");
