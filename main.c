da10f4d7ad6749a18fe0caff1622fe7e2fd34e6c
ö <daveyeb@gmail.com>
2021-03-01 13:44:26 -0500

    9

--- main.c
+++ main.c
@@ -4,19 +4,17 @@
 
 int main()
 {
-	wheels *cash;
-	plugboard *money;
+	_enigma_wheels *cash;
+	_enigma_plugboard *money;
 
-	int a = 0;
-
-	cash = malloc(sizeof(wheels));
-	money = malloc(sizeof(plugboard));
-	a = config_wheel_order(cash, "B I II III");
+	cash = malloc(sizeof(_enigma_wheels));
+	money = malloc(sizeof(_enigma_plugboard));
+	int a = config_wheel_order(cash, "B I II III");
 	a = config_start_pos_rotors(cash, "A A A");
 	a = config_plug_connections(money, "");
 	a = config_reflector_wiring(cash, "BETA", "IU AS DV GL FT OX EZ CH MR KN BQ PW");
 	a = config_ring_settings(cash, "1 1 1");
-	a = encrypt(cash, money, "B");
+	a = encrypt(cash, money, "MDQQI");
 
 	printf("\na = %d\n", a);
 
